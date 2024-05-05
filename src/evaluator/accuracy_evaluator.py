62c8b0a3ae3abd056fb45519e348dfd0749705ee
Dmitrii Belik <belik820@gmail.com>
2024-05-05 15:04:54 +0200

    Fix import

--- src/evaluator/accuracy_evaluator.py
+++ src/evaluator/accuracy_evaluator.py
@@ -1,6 +1,7 @@
 import random
 import torch
-from torch.utils.data import DataLoader, pad_sequence
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 
@@ -16,41 +17,42 @@ class AccuracyEvaluator:
         self.model.to(self.device)
 
     def evaluate_accuracy(self):
-            accuracies = []
-            test_dataloader = DataLoader(self.test_dataset, batch_size=self.batch_size)
+        accuracies = []
+        test_dataloader = DataLoader(self.test_dataset, batch_size=self.batch_size)
 
-            padding_value = self.tokenizer.pad_token_id if self.tokenizer.pad_token_id is not None else 0
+        padding_value = self.tokenizer.pad_token_id if self.tokenizer.pad_token_id is not None else 0
 
-            for batch in tqdm(test_dataloader):
-                input_ids_batch = batch["input_ids"]
-                max_length = max(len(input_ids) for input_ids in input_ids_batch)
+        for batch in tqdm(test_dataloader):
+            input_ids_batch = batch["input_ids"]
+            max_length = max(len(input_ids) for input_ids in input_ids_batch)
 
-                masked_input_ids_batch = []
+            masked_input_ids_batch = []
 
-                for input_ids in input_ids_batch:
-                    input_ids = torch.tensor(input_ids)
-                    for _ in range(self.num_samples):
-                        mask_index = random.randint(0, len(input_ids) - 1)
-                        masked_input_ids = input_ids[:mask_index].clone().detach()
+            for input_ids in input_ids_batch:
+                input_ids = torch.tensor(input_ids)
+                for _ in range(self.num_samples):
+                    mask_index = random.randint(0, len(input_ids) - 1)
+                    masked_input_ids = input_ids[:mask_index].clone().detach()
 
-                        masked_input_ids_batch.append(masked_input_ids)
+                    masked_input_ids_batch.append(masked_input_ids)
 
-                masked_input_ids_batch = pad_sequence(masked_input_ids_batch, batch_first=True, padding_value=padding_value).to(self.device)
+            masked_input_ids_batch = pad_sequence(masked_input_ids_batch, batch_first=True,
+                                                  padding_value=padding_value).to(self.device)
 
-                with torch.no_grad():
-                    outputs = self.model.generate(
-                        input_ids=masked_input_ids_batch,
-                        max_length=max_length,
-                        num_return_sequences=1,
-                        pad_token_id=self.tokenizer.pad_token_id
-                    )
+            with torch.no_grad():
+                outputs = self.model.generate(
+                    input_ids=masked_input_ids_batch,
+                    max_length=max_length,
+                    num_return_sequences=1,
+                    pad_token_id=self.tokenizer.pad_token_id
+                )
 
-                    for i in range(len(input_ids_batch)):
-                        for j in range(self.num_samples):
-                            output_index = i * self.num_samples + j
-                            predicted_ids = outputs[output_index][len(masked_input_ids_batch[output_index]):].cpu()
-                            input_ids = input_ids_batch[i]
-                            accuracy = (predicted_ids == input_ids).float().mean().item()
-                            accuracies.append(accuracy)
+                for i in range(len(input_ids_batch)):
+                    for j in range(self.num_samples):
+                        output_index = i * self.num_samples + j
+                        predicted_ids = outputs[output_index][len(masked_input_ids_batch[output_index]):].cpu()
+                        input_ids = input_ids_batch[i]
+                        accuracy = (predicted_ids == input_ids).float().mean().item()
+                        accuracies.append(accuracy)
 
-            return sum(accuracies) / len(accuracies)
+        return sum(accuracies) / len(accuracies)
